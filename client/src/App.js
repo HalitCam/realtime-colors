77dceb94eaefd99170cd84a495a6efc1c3cbd4bd
Halit <xltcamm@gmail.com>
2025-09-09 21:09:21 +0200

    client folder again reproduced

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,25 @@
-import "./App.css";
-
-import { useEffect, useState } from "react";
-import { init, subscribe } from "./socketApi";
-import Palatte from "./components/Palatte";
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-	const [activeColor, setActiveColor] = useState("#282c34");
-
-	useEffect(() => {
-		init();
-
-		subscribe((color) => {
-			setActiveColor(color);
-		});
-	}, []);
-
-	return (
-		<div className="App" style={{ backgroundColor: activeColor }}>
-			<h1>{activeColor}</h1>
-			<Palatte activeColor={activeColor} />
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
